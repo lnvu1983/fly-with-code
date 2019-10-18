46228f2b208a614fb3393b6e9010cb34e83468b9
lnvu <nguyenvuluu@vneon.net>
2019-10-18 15:08:41 +0700

    Adding content, modify code

--- js/script.js
+++ js/script.js
@@ -1,14 +1,39 @@
+function empty(obj) {
+	const objType = typeof(obj);
+
+	if (objType === 'undefined' || obj === null) {
+		return true;
+	}
+
+	if (objType === 'object') {
+		// JSON
+		if (isNaN(parseInt(obj.length, 10))) {
+			return (JSON.stringify(obj) === JSON.stringify({}));
+		}
+		// List
+		else {
+			return (obj.length === 0);
+		}
+	}
+
+	if (objType === 'string') {
+		return ($.trim(obj) === '');
+	}
+
+	return false;
+}
+
 function initPlan() {
 	$.getJSON('data/plan.json', function(data) {
-		const planObject = $('.plans');
+		const planObject = $('#plan');
 		const template = planObject.find('.item.template');
 		const container = template.parent();
 
-		template.removeClass("template");
+		template.removeClass('template');
 
 		data.forEach(function (item, index) {
-			const itemObject = template.clone();
-			const title = index == 2 ? 'Next plan' : (index == 1 ? 'Current plan' : 'Last plan');
+			let itemObject = template.clone();
+			let title = index == 2 ? 'Next plan' : (index == 1 ? 'Current plan' : 'Last plan');
 
 			itemObject.find('.title').html(title);
 			itemObject.find('.date').html(item.date);
@@ -49,6 +74,34 @@ function calcBannerTop() {
 	$('header .banner').css('padding-top', navHeight);
 }
 
+function initMembers() {
+	$.getJSON('data/mem.json', function(data) {
+		const memObject = $('#members');
+		const template = memObject.find('.mem-list .item.template');
+		const container = template.parent();
+
+		template.removeClass('template');
+
+		data.forEach(function (item, index) {
+			let itemObject = template.clone();
+			let avatar = empty(item.avatar) ? 'no-avatar.jpg' : item.avatar;
+			let quote = empty(item.quote) ? 'I\'m too lazy to upload an avatar & write something good' : item.quote;
+
+			itemObject.find('img').attr('src', 'img/mem/' + avatar);
+			itemObject.find('.name').html(item.name);
+			itemObject.find('.quote').html(quote);
+
+			container.append(itemObject);
+		});
+
+		template.remove();
+
+		memObject.removeClass('hide');
+
+		initMemberSlider();
+	});
+}
+
 $(document).ready(function() {
 	$(window).resize(function(e) {
 		calcBannerTop();
@@ -56,7 +109,7 @@ $(document).ready(function() {
 
 	initScrollAnimation();
 
-	initMemberSlider();
+	initMembers();
 	initPlan();
 
 	calcBannerTop();
